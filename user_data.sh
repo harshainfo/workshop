cadbc25dfc631dcb1ca5919e4cfbde165defdb86
Harsha Pitawela <harshainfo@gmail.com>
2023-07-07 14:57:25 -0400

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -3,6 +3,10 @@
 sudo amazon-linux-extras install epel
 # Install and start Apache web server
 sudo yum install -y httpd php
+# update permissions
+sudo chmod 777 /var/www/html
+# index.php to return page response with 200
+echo "Hello World" > /var/www/html/index.php
 # Start the httpd service
 service httpd start
 # Install CPU stress test tool
